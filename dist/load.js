e3b8c81d50cf3153f1614a64066a5171785a9647
Tougaj <sashamolodec@gmail.com>
2020-01-11 17:47:47 +0200

    Добавлена поддержка прокси-сервера

--- dist/load.js
+++ dist/load.js
@@ -9,25 +9,44 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 const fs = require('fs');
+const path = require('path');
 const axios = require('axios');
+const httpsProxyAgent = require('https-proxy-agent');
 const data = {
     headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0'
     },
-    outPath: './fonts/',
+    outPath: './fonts',
 };
 const argv = require('yargs')
-    .usage('Usage: $0 -s [str]')
+    .usage('Usage: $0 -s [str] -p [str]')
     .demandOption(['s'])
     .alias('s', 'source')
     .nargs('s', 1)
-    .string((['s']))
+    .string((['s', 'p']))
     .describe('s', 'Link to Goggle Fonts css file')
+    .alias('p', 'proxy')
+    .nargs('p', 1)
+    .describe('p', 'Proxy configuration in format http://login:password@address:port/')
     .help('h')
     .alias('h', 'help')
     .argv;
 const cssLink = argv.source;
-fs.access(data.outPath, fs.constants.F_OK, (err) => err && fs.mkdirSync(data.outPath));
+const proxyAddress = argv.proxy;
+const agent = proxyAddress ? new httpsProxyAgent(proxyAddress) : undefined;
+const outPath = data.outPath;
+const removeOldFonts = (directory) => {
+    fs.readdir(directory, (err, files) => {
+        if (err)
+            throw err;
+        for (const file of files) {
+            fs.unlink(path.join(directory, file), (err) => {
+                if (err)
+                    throw err;
+            });
+        }
+    });
+};
 const getFontsLinks = (cssLink) => {
     return new Promise((resolve, reject) => {
         axios({
@@ -35,10 +54,11 @@ const getFontsLinks = (cssLink) => {
             url: cssLink,
             responseType: 'text',
             headers: data.headers,
+            httpsAgent: agent,
         })
             .then((response) => response.data)
             .then((text) => {
-            fs.writeFileSync(`${data.outPath}/style.css`, text.replace(/https:\/\/fonts.gstatic.com\/s\/roboto\/v20\//ig, ''));
+            fs.writeFileSync(path.join(outPath, 'style.css'), text.replace(/https:\/\/fonts.gstatic.com[^)]+\/([^/)]+)/ig, '$1'));
             const re = new RegExp(/url\((https:\/\/fonts\.[^)]+)\)/ig);
             const fontLinks = [];
             let ar;
@@ -62,9 +82,10 @@ const loadFont = (link) => {
             url: link,
             responseType: 'stream',
             headers: data.headers,
+            httpsAgent: agent,
         })
             .then((response) => {
-            response.data.pipe(fs.createWriteStream(`${data.outPath}${fileName}`)
+            response.data.pipe(fs.createWriteStream(path.join(outPath, fileName))
                 .on('finish', () => {
                 resolve(`${fileName} loaded`);
             }));
@@ -79,6 +100,17 @@ const loadFonts = (fontLinks) => __awaiter(void 0, void 0, void 0, function* () 
         console.log(result);
     }
     return 'All fonts loaded';
+});
+fs.access(outPath, fs.constants.F_OK, (err) => {
+    if (err) {
+        fs.mkdir(outPath, (err) => {
+            if (err)
+                throw err;
+        });
+    }
+    else {
+        removeOldFonts(outPath);
+    }
 });
 getFontsLinks(cssLink)
     .then((fontLinks) => {
--- src/load.ts
+++ src/load.ts
@@ -1,29 +1,48 @@
 const fs = require('fs');
+const path = require('path');
 const axios = require('axios');
+const httpsProxyAgent = require('https-proxy-agent');
 
 const data = {
-    headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0'
-    },
-    outPath: './fonts/',
+	headers: {
+		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0'
+	},
+	outPath: './fonts',
 }
 
 const argv = require('yargs')
-    .usage('Usage: $0 -s [str]')
-    .demandOption(['s'])
-    .alias('s', 'source')
-    .nargs('s', 1)
-    .string((['s']))
-    .describe('s', 'Link to Goggle Fonts css file')
-    .help('h')
-    .alias('h', 'help')
-    .argv;
+	.usage('Usage: $0 -s [str] -p [str]')
+	.demandOption(['s'])
+	.alias('s', 'source')
+	.nargs('s', 1)
+	.string((['s', 'p']))
+	.describe('s', 'Link to Goggle Fonts css file')
+	.alias('p', 'proxy')
+	.nargs('p', 1)
+	.describe('p', 'Proxy configuration in format http://login:password@address:port/')
+	// .alias('d', 'directory')
+	// .nargs('d', 1)
+	// .describe('d', 'Directory name in fonts folder where fonts will be loaded')
+	.help('h')
+	.alias('h', 'help')
+	.argv;
 
 const cssLink = argv.source;
-// const cssLink = 'https://fonts.googleapis.com/css?family=Roboto:400,400i,500&display=swap&subset=cyrillic,cyrillic-ext';
-// const agent = new httpsProxyAgent('http://login:password@192.168.0.1:3128/');
+const proxyAddress = argv.proxy;
+const agent = proxyAddress ? new httpsProxyAgent(proxyAddress) : undefined;
+// const outPath = path.join(data.outPath, argv.directory || '');
+const outPath = data.outPath;
 
-fs.access(data.outPath, fs.constants.F_OK, (err: Error) => err && fs.mkdirSync(data.outPath));
+const removeOldFonts = (directory: string) => {
+	fs.readdir(directory, (err: Error, files: string[]) => {
+		if (err) throw err;
+		for (const file of files) {
+			fs.unlink(path.join(directory, file), (err: Error) => {
+				if (err) throw err;
+			});
+		}
+	});
+}
 
 const getFontsLinks = (cssLink: string): Promise<string[]> => {
     return new Promise((resolve, reject) => {
@@ -32,11 +51,11 @@ const getFontsLinks = (cssLink: string): Promise<string[]> => {
             url: cssLink,
             responseType: 'text',
             headers: data.headers,
-            // httpsAgent: agent,
+            httpsAgent: agent,
         })
         .then((response: any) => response.data)
         .then((text: string) => {
-            fs.writeFileSync(`${data.outPath}/style.css`, text.replace(/https:\/\/fonts.gstatic.com\/s\/roboto\/v20\//ig, ''));
+            fs.writeFileSync(path.join(outPath, 'style.css'), text.replace(/https:\/\/fonts.gstatic.com[^)]+\/([^/)]+)/ig, '$1'));
             const re = new RegExp(/url\((https:\/\/fonts\.[^)]+)\)/ig);
             const fontLinks: string[] = [];
             let ar: RegExpExecArray | null;
@@ -62,11 +81,11 @@ const loadFont = (link: string) => {
             url: link,
             responseType:'stream',
             headers: data.headers,
-            // httpsAgent: agent,
+            httpsAgent: agent,
         })
         .then((response: any) => {
             response.data.pipe(
-                fs.createWriteStream(`${data.outPath}${fileName}`)
+                fs.createWriteStream(path.join(outPath, fileName))
                     .on('finish', () => {
                         resolve(`${fileName} loaded`);
                     })
@@ -78,7 +97,7 @@ const loadFont = (link: string) => {
 
 const loadFonts = async (fontLinks: string[]) => {
     console.log(`Total fonts: ${fontLinks.length}`);
-    
+	
     for (let index = 0; index < fontLinks.length; index++) {
         const link = fontLinks[index];
         const result = await loadFont(link);
@@ -86,6 +105,17 @@ const loadFonts = async (fontLinks: string[]) => {
     }
     return 'All fonts loaded';
 }
+
+// Если каталога нет, то создаем его, иначе удаляем из него все файлы
+fs.access(outPath, fs.constants.F_OK, (err: Error) => {
+	if (err){
+		fs.mkdir(outPath, (err: Error) => {
+			if (err) throw err;
+		});
+	} else {
+		removeOldFonts(outPath);
+	}
+});
 
 getFontsLinks(cssLink)
     .then((fontLinks: string[]) => {
